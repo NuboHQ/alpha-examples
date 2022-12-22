045d6598f935f04143bb28015875141da8e37eaa
Marcel Thomas <marcelthomas5@gmail.com>
2022-12-22 17:02:51 +0000

    Update hello.ts

--- functions/hello.ts
+++ functions/hello.ts
@@ -3,7 +3,6 @@ export const path = '/';
 export const handler = () => {
   return {
     message: `Hello from Nubo Edge Function`,
-    test: 'hi there',
     region: process.env.NUBO_REGION,
   };
 };
