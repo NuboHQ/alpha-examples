c637c6c5a1e5680291fa41f3f3eac0c49742ed05
Marcel Thomas <marcelthomas5@gmail.com>
2022-12-27 11:55:46 +0000

    Updated html

--- bun/src/index.ts
+++ bun/src/index.ts
@@ -1,6 +1,5 @@
 import { Hono } from 'hono';
 import { serveStatic } from 'hono/serve-static.bun';
-import Page from './components/Page';
 
 const port = parseInt(process.env.PORT) || 5000;
 const app = new Hono();
@@ -12,8 +11,26 @@ app.use('/bun.svg', serveStatic({ path: './public/bun.svg' }));
 app.get('/hello', (c) => {
   return c.json({ message: 'Hello from Bun', region: process.env.NUBO_REGION });
 });
+
 app.get('*', (c) => {
-  return c.html(<Page region={process.env.NUBO_REGION} />);
+  return c.html(`<html>
+      <head>
+        <link rel="stylesheet" href="global.css" />
+        <title>Bun on Nubo</title>
+      </head>
+
+      <body>
+        <div class="box">
+          <div>
+            <img class="bun-logo" src="bun.svg" alt="Bun" />
+
+            <h1>Bun on Nubo</h1>
+
+            <p>Region: ${process.env.NUBO_REGION}</p>
+          </div>
+        </div>
+      </body>
+    </html>`);
 });
 
 console.log(`Running at http://localhost:${port}`);
